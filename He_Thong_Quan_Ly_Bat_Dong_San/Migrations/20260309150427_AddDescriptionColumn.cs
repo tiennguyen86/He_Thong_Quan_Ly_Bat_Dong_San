c89b44046a6c299b98392a50deb464a83629a390
tiennguyen <tien86291@gmail.com>
2026-03-16 21:38:38 +0700

    ok lun

---
+++ He_Thong_Quan_Ly_Bat_Dong_San/Migrations/20260309150427_AddDescriptionColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace He_Thong_Quan_Ly_Bat_Dong_San.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Properties");
+        }
+    }
+}
